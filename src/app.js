4e43011871f946ab0b6a1c8b5ee4861137e2d547
Csajbok-Remenyi Laszlo <csajbokremenyilaszlo@ktch.hu>
2023-03-02 19:49:48 +0100

    Folytatás

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 const tbody = document.querySelector("#tbody");
-const saveButton=document.querySelector('#savebutton');
-const nameInput =document.querySelector('#name');
-const quantityInput =document.querySelector('#quantity');
-const priceInput =document.querySelector('#price');
+const saveButton = document.querySelector('#savebutton');
+const nameInput = document.querySelector('#name');
+const quantityInput = document.querySelector('#quantity');
+const priceInput = document.querySelector('#price');
+
+const idEdit = document.querySelector('#editid');
+const nameEdit = document.querySelector('#editname');
+const quantityEdit = document.querySelector('#editquantity');
+const priceEdit = document.querySelector('#editprice');
+const editButton = document.querySelector('#saveEditButton');
 
 const gyumolcsok = [
     { id: 1, name: 'szilva', quantity: 35, price: 8 },
     { id: 2, name: 'alma', quantity: 45, price: 8.3 },
     { id: 3, name: 'körte', quantity: 25, price: 9.5 },
     { id: 4, name: 'barack', quantity: 37, price: 12 }
-  ];
+];
 
 
 function generateTbody() {
@@ -26,8 +32,9 @@ function generateTbody() {
         tbody.append(tr);
         tr.append(tdName);
         tr.append(tdQuantity);
-        tr.append(tdPrice);        
+        tr.append(tdPrice);
         tr.append(generateTdDelete(gyumolcs.id));
+        tr.append(generateTdEdit(gyumolcs));
     });
 }
 generateTbody();
@@ -42,7 +49,7 @@ function generateTdDelete(id) {
         let index = 0;
         let count = 0;
         gyumolcsok.forEach((gy) => {
-            if(gy.id == id) {
+            if (gy.id == id) {
                 index = count;
             }
             count++;
@@ -56,18 +63,63 @@ function generateTdDelete(id) {
     return td;
 }
 
-saveButton.addEventListener('click', ()=>{
+saveButton.addEventListener('click', () => {
     console.log('működik');
-    let name=nameInput.value;
-    let quantity=quantityInput.value;
-    let price=priceInput.value;
-    let gyumolcs={
-        name:name,
-        quantity:quantity,
-        price:price
-    }
+    let name = nameInput.value;
+    let quantity = quantityInput.value;
+    let price = priceInput.value;
+    let gyumolcs = {
+        name: name,
+        quantity: quantity,
+        price: price
+    };
+
     gyumolcsok.push(gyumolcs);
     console.log(gyumolcsok);
-    tbody.textContent='';
+    tbody.textContent = '';
+    generateTbody();
+    clearInputOnAddModal();
+});
+
+function clearInputOnAddModal() {
+
+    nameInput.value = '';
+    quantityInput.value = '';
+    priceInput.value = '';
+}
+
+editButton.addEventListener('click', () => {
+    let id = idEdit.value;
+    let name = nameEdit.value;
+    let quantity = quantityEdit.value;
+    let price = priceEdit.value;
+
+    gyumolcsok.forEach(gyumolcs => {
+        if ((gyumolcs.id == id)) {
+            gyumolcs.name = name;
+            gyumolcs.price = price;
+            gyumolcs.quantity = quantity;
+        }
+    });
+    tbody.textContent = '';
     generateTbody();
 });
+
+
+function generateTdEdit(fruit) {
+    let td = document.createElement('td');
+    let button = document.createElement('button');
+    button.textContent = "Módosítás";
+    button.classList = "btn btn-secondary";
+    button.setAttribute('data-bs-toggle', 'modal');
+    button.setAttribute('data-bs-target', "#editModal");
+    button.addEventListener('click', () => {
+        idEdit.value = fruit.id;
+        nameEdit.value = fruit.name;
+        quantityEdit.value = fruit.quantity;
+        priceEdit.value = fruit.price;
+
+    });
+    td.append(button);
+    return td;
+}
